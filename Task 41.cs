5c9bedaf56fceb41720728d8edb7a314c35094fa
Daniar <tbylat@gmail.com>
2022-10-21 16:58:52 +0500

    New commit

---
+++ Task 41.cs
@@ -0,0 +1,31 @@
+﻿//Задача 41: Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+// Console.WriteLine("Введите кол-во чисел");
+// int size = Convert.ToInt32(Console.ReadLine());
+// int result = 0;
+// int [] array = new int [size];
+// for (int i = 0; i < array.Length; i++)
+// {
+//     Console.WriteLine("Введите числа ");
+//     array[i] = Convert.ToInt32(Console.ReadLine().ToString());
+// ;
+// }
+// printArray(array);
+// void printArray(int[] array)
+// {
+//     Console.Write($"[{array[0]}");
+//     for (int i = 1; i < array.Length; i++)
+//         Console.Write($", {array[i]}");
+//     Console.WriteLine($"]");
+// }
+// void resultat(int result)
+// {
+//     for (int i = 0; i < array.Length; i++)
+//     {
+//         if (array[i] > 0)
+//         {
+//             result = result + 1;
+//         }
+//     }
+//     Console.WriteLine("Чисел больше 0: " + result);
+// }
+// resultat(result);
---
+++ Task 43.cs
@@ -0,0 +1,21 @@
+﻿// Задача 43: Напишите программу, которая найдёт точку пересечения двух прямых, заданных уравнениями y = k1 * x + b1, y = k2 * x + b2;
+//  значения b1, k1, b2 и k2 задаются пользователем.
+// Console.Write("Введите значение координаты k1 первой точки: ");
+// double k1 = Convert.ToInt32(Console.ReadLine());
+// Console.Write("Введите значение координаты b1 первой точки: ");
+// double b1 = Convert.ToInt32(Console.ReadLine());
+// Console.Write("Введите значение координаты k2 второй точки: ");
+// double k2 = Convert.ToInt32(Console.ReadLine());
+// Console.Write("Введите значение координаты b2 второй точки: ");
+// double b2 = Convert.ToInt32(Console.ReadLine());
+// double x = (b2 - b1)/(k1 - k2);
+// double y = k1 * x + b1;
+// if (k2 == k1)
+// {
+//     Console.WriteLine("Не пересекутся");
+// }
+// else
+// {
+//     Console.WriteLine($"Прямые пересекутся в точке с координатами ({x},{y})");
+// }
+
